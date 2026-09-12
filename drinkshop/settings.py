056d5a91463f7f93582674bffe7952386eab2a57
radical-vicky <entreprenuerridicular@gmail.com>
2026-09-12 18:12:21 +0300

    first commit

--- drinkshop/settings.py
+++ drinkshop/settings.py
@@ -255,7 +255,7 @@ if EMAIL_HOST and not DEBUG:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'RadicaldrinkShop <info.radicaldrinkshp@gmail.com>')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'RadicaldrinkShop <info.radicaldrinkshop@gmail.com>')
 
 # ---------------------------------------------------------------------------
 # Shop settings
